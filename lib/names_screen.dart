f3b2631c59ed8b7262f8b789d34822402c3893da
Adarsh Jain <jadarsh00@gmail.com>
2022-03-19 18:08:00 +0530

    namescreen text boxes made

--- lib/names_screen.dart
+++ lib/names_screen.dart
@@ -1,15 +1,46 @@
 import 'package:flutter/material.dart';
 
 class NamesScreen extends StatefulWidget {
-  const NamesScreen({Key? key}) : super(key: key);
+  final int numPlayers;
+  const NamesScreen({Key? key, required this.numPlayers}) : super(key: key);
 
   @override
   _NamesScreenState createState() => _NamesScreenState();
 }
 
 class _NamesScreenState extends State<NamesScreen> {
+  late List controllers;
+  _NamesScreenState() {
+    controllers = [];
+    for (var i = 0; i < widget.numPlayers; i++) {
+      controllers.insert(i, TextEditingController());
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Player Names"),
+      ),
+      body: Column(
+        children: [
+          ListView.builder(
+            itemCount: widget.numPlayers,
+            itemBuilder: (BuildContext context, int index) {
+              return Row(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Text("Player" + index.toString()),
+                  TextFormField(
+                    controller: controllers[index],
+                  )
+                ],
+              );
+            },
+          ),
+        ],
+      ),
+    );
   }
 }
