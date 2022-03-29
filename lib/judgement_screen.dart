ff18505eb8cbdc8d57498a320998c39966f990c8
Adarsh Jain <jadarsh00@gmail.com>
2022-03-29 18:23:35 +0530

    Judgement screen validation added

--- lib/judgement_screen.dart
+++ lib/judgement_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertut/game_singletone.dart';
 import 'package:fluttertut/players_singleton.dart';
 import 'package:fluttertut/round_screen.dart';
 
@@ -11,65 +12,114 @@ class JudgementScreen extends StatefulWidget {
 
 class _JudgementScreenState extends State<JudgementScreen> {
   late List<TextEditingController> _controllers;
-  late List<int> _playerChoices;
-  late List<TableRow> _rows;
   @override
   void initState() {
     super.initState();
     _controllers = [];
+    for (var i = 0; i < PlayerSingletone().numPlayers; i++) {
+      _controllers.insert(i, TextEditingController());
+    }
+  }
+
+  bool roundScoreSumValid = true;
+
+  String? validRoundScore(String? scoreString) {
+    if (scoreString == null) {
+      return "Enter Valid Number";
+    }
+    int? score = int.tryParse(scoreString);
+    if (score == null) {
+      return "Enter Valid Number";
+    }
+    return null;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    List<int> _playerChoices;
+    List<TableRow> _rows;
+    GlobalKey<FormState> _formkey = GlobalKey();
     _rows = [];
     _playerChoices = [];
     for (var i = 0; i < PlayerSingletone().numPlayers; i++) {
-      _controllers.insert(i, TextEditingController());
       _rows.add(TableRow(children: [
         Text(
           PlayerSingletone().playerNames[i],
           textAlign: TextAlign.center,
         ),
         Container(),
-        TextFormField(
-          controller: _controllers[i],
-          textInputAction: TextInputAction.next,
-          keyboardType: TextInputType.number,
-        )
+        Container(
+          child: TextFormField(
+            controller: _controllers[i],
+            textInputAction: TextInputAction.next,
+            keyboardType: TextInputType.number,
+            textAlign: TextAlign.center,
+          ),
+          padding: const EdgeInsets.all(15.0),
+        ),
+        Container(),
       ]));
       _playerChoices.add(0);
     }
-  }
-
-  @override
-  Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: const Text("Judge Your cards"),
+      ),
       body: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            Table(
-              columnWidths: const <int, TableColumnWidth>{
-                0: FractionColumnWidth(0.5),
-                1: FractionColumnWidth(0.25),
-                2: FractionColumnWidth(0.25)
-              },
-              defaultVerticalAlignment: TableCellVerticalAlignment.middle,
-              children: _rows,
-            ),
-            ButtonTheme(
-                child: ElevatedButton(
-              onPressed: () {
-                for (var i = 0; i < PlayerSingletone().numPlayers; i++) {
-                  _playerChoices[i] =
-                      int.parse(_controllers[i].text.toString().trim());
-                }
-                Navigator.pushReplacement(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) =>
-                          RoundScreen(playerChoices: _playerChoices),
-                    ));
-              },
-              child: const Text("Start Game"),
-            ))
-          ],
+        child: Form(
+          key: _formkey,
+          child: Column(
+            children: <Widget>[
+              Table(
+                columnWidths: const <int, TableColumnWidth>{
+                  0: FractionColumnWidth(0.6),
+                  1: FractionColumnWidth(0.0),
+                  2: FractionColumnWidth(0.2),
+                  3: FractionColumnWidth(0.2)
+                },
+                defaultVerticalAlignment: TableCellVerticalAlignment.middle,
+                children: _rows,
+              ),
+              Container(
+                  child: Text("Cards Distributed : " +
+                      GameSingletone().maxCards.toString()),
+                  padding: const EdgeInsets.all(10.0),
+                  decoration: BoxDecoration(
+                      border: Border.all(),
+                      borderRadius: BorderRadius.circular(5.0))),
+              ButtonTheme(
+                  child: ElevatedButton(
+                onPressed: () {
+                  if (_formkey.currentState!.validate()) {
+                    int judgmentSum = 0;
+                    for (var i = 0; i < PlayerSingletone().numPlayers; i++) {
+                      _playerChoices[i] =
+                          int.parse(_controllers[i].text.toString().trim());
+                      judgmentSum += _playerChoices[i];
+                    }
+                    if (judgmentSum == GameSingletone().maxCards) {
+                      setState(() {
+                        roundScoreSumValid = false;
+                        _formkey.currentState!.save();
+                      });
+                      return;
+                    }
+                    Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) =>
+                              RoundScreen(playerChoices: _playerChoices),
+                        ));
+                  }
+                },
+                child: const Text("Start Game"),
+              )),
+              roundScoreSumValid
+                  ? Container()
+                  : Text("Sum of judgments should not be " +
+                      GameSingletone().maxCards.toString())
+            ],
+          ),
         ),
       ),
     );
